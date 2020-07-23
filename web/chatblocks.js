6bb9f5e2dbf58b84dc1dcf74d3b10f225087baf3
Instafluff <35773245+instafluff@users.noreply.github.com>
2020-07-23 11:59:18 -0700

    Revert "Async Functions"
    This reverts commit 374364ff6c069c06caf5ff883b9aa600aa22cbbc.

--- web/chatblocks.js
+++ web/chatblocks.js
@@ -134,7 +134,7 @@ function codeUpdateHandler( event ) {
 		<script type="text/javascript">
 		math.createUnit('tbsp', {definition: '1 tablespoon', aliases: ["tbspoon"]});
 		math.createUnit('tsp', {definition: '1 teaspoon', aliases: ["tspoon"]});
-
+		
 		let user = undefined, command = undefined, flags = {}, extra = {};
 		let onCommandHandlers = {};
 		let onChatHandlers = [];
@@ -215,8 +215,6 @@ function codeUpdateHandler( event ) {
 		</script>
 	</body>
 </html>`;
-
-	theCode = theCode.replace( /function/g, "async function" );
 
 	let xml = Blockly.Xml.workspaceToDom( Blockly.getMainWorkspace() );
 	window.localStorage.setItem( "ChatBlocks", Blockly.Xml.domToText( xml ) );
@@ -1168,7 +1166,7 @@ Blockly.JavaScript["text_replace"] = function(block) {
 	var value_replace = Blockly.JavaScript.valueToCode(block, "REPLACE", Blockly.JavaScript.ORDER_ATOMIC) || '';
 	var value_with = Blockly.JavaScript.valueToCode(block, "WITH", Blockly.JavaScript.ORDER_ATOMIC) || '';
 	var value_input = Blockly.JavaScript.valueToCode(block, "INPUT", Blockly.JavaScript.ORDER_ATOMIC) || '';
-
+	
 	var code = `${value_input}.replace(${value_replace}, ${value_with})`;
 
 	return [code, Blockly.JavaScript.ORDER_NONE];
@@ -1214,7 +1212,7 @@ Blockly.JavaScript["text_replaceAll"] = function(block) {
 	var value_replace = Blockly.JavaScript.valueToCode(block, "REPLACE", Blockly.JavaScript.ORDER_ATOMIC) || '';
 	var value_with = Blockly.JavaScript.valueToCode(block, "WITH", Blockly.JavaScript.ORDER_ATOMIC) || '';
 	var value_input = Blockly.JavaScript.valueToCode(block, "INPUT", Blockly.JavaScript.ORDER_ATOMIC) || '';
-
+	
 	var code = `${value_input}.replaceAll(${value_replace}, ${value_with})`;
 
 	return [code, Blockly.JavaScript.ORDER_NONE];
@@ -1604,12 +1602,12 @@ Blockly.Blocks[ "json_create_with" ] = {
     container.setAttribute('items', this.itemCount_);
     return container;
 	},
-
+	
 	domToMutation: function(xmlElement) {
     this.itemCount_ = parseInt(xmlElement.getAttribute('items'), 10);
     this.updateShape_();
 	},
-
+	
 	decompose: function(workspace) {
     var containerBlock = workspace.newBlock('json_create_with_container');
     containerBlock.initSvg();
@@ -1622,7 +1620,7 @@ Blockly.Blocks[ "json_create_with" ] = {
     }
     return containerBlock;
 	},
-
+	
 	compose: function(containerBlock) {
     var itemBlock = containerBlock.getInputTargetBlock('STACK');
     // Count number of inputs.
@@ -1646,7 +1644,7 @@ Blockly.Blocks[ "json_create_with" ] = {
 			Blockly.Mutator.reconnect(connections[i], this, 'KEY' + i);
     }
 	},
-
+	
 	saveConnections: function(containerBlock) {
     var itemBlock = containerBlock.getInputTargetBlock('STACK');
     var i = 0;
