8fda7d69d8916b5aa8c2a99bb8b7fe10e8da21f8
instafluff <instaflufftv@gmail.com>
2020-09-05 02:41:58 -0700

    Revert "search feature (#47)"
    This reverts commit 0b9e2021db6617a701b7c10dd17e1bb3715bec78.

--- web/chatblocks.js
+++ web/chatblocks.js
@@ -127,8 +127,6 @@ var workspace = Blockly.inject( "blocklyDiv", {
 	oneBasedIndex: false,
 } );
 
-const workspaceSearch = new WorkspaceSearch(workspace).init();
-
 let theCode = "";
 
 function codeUpdateHandler( event ) {
